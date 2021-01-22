deee1d3f152d74284aae364edfeb8b00acdf58fa
“Kwadwo <ka2526@columbia.edu>
2021-01-21 20:57:12 -0600

    January 21, 2021 commits

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+/*
+
+Author :Kwadwo Adutwum , Nicknamee: Optimistic Email:ka2526@columbia.edu
+This is a responsive web project build from scratch using HTML,CSS,JavaScript
+This is a self-taught exercise aimed at implementing concepts into full project
+Edits and notes will be incrementally added as and when needed
+
+Mac shortcut for file paths : right-click on file, Shift+Option reveals path
+CTRL-SHIFT-H shortcut key to preview HTML text file in atom text editor
+
+*/
